fc6842d9fc42115059352ccb8dbd2a03f5ff492c
Pixel48 <piksel48@gmail.com>
2022-01-12 12:51:01 +0100

    Add basic rotation

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,45 @@
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(1920, 1080), "ONAS Project", sf::Style::Close | sf::Style::Fullscreen);
-  sf::Texture BackgroundTexture;
-  sf::Sprite BackgroundSprite;
-  if(!BackgroundTexture.loadFromFile("./img/N.png")) {
-    printf("Error loading background.png\n");
+  // window.setFramerateLimit(15);
+
+  sf::Texture N;
+  if(!N.loadFromFile("img/N.png")) {
+    printf("N.png not found!\n");
     return 1;
   }
-  BackgroundSprite.setTexture(BackgroundTexture);
+  sf::Texture E;
+  if(!E.loadFromFile("img/E.png")) {
+    printf("E.png not found!\n");
+    return 1;
+  }
+  sf::Texture S;
+  if(!S.loadFromFile("img/S.png")) {
+    printf("S.png not found!\n");
+    return 1;
+  }
+  sf::Texture W;
+  if(!W.loadFromFile("img/W.png")) {
+    printf("W.png not found!\n");
+    return 1;
+  }
 
-  sf::Texture texture;
-  texture.loadFromFile("./img/N.png");
+  // for(int i = 0; i < 4; i++) {
+  //   screen[i] = (char*)malloc(sizeof(char) * 11);
+  // }
+  // screen[0] = "img/N.png";
+  // screen[1] = "img/E.png";
+  // screen[2] = "img/S.png";
+  // screen[3] = "img/W.png";
+  int screenView = 0;
+
+  // sf::Texture BackgroundTexture;
+  sf::Sprite bg;
+  // if(!BackgroundTexture.loadFromFile(screen[screenView])) {
+  //   printf("Error loading background.png\n");
+  //   return 1;
+  // }
+  // bg.setTexture(N);
 
   Area moveLeft(-1, 0, 101, 1080);
   Area moveRight(1920-100, 0, 101, 1080);
@@ -39,12 +68,35 @@ int main() {
       if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)
       || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q)) window.close();
 
-      if(moveLeft.isEntering()) printf("Left\n");
-      if(moveRight.isEntering()) printf("Right\n");
+      if(moveLeft.isEntering()) {
+        printf("Left\n");
+        screenView--;
+        if(screenView < 0) screenView = 3;
+      }
+      if(moveRight.isEntering()) {
+        printf("Right\n");
+        screenView++;
+        screenView %= 4;
+      }
+    }
+
+    switch(screenView) {
+      case 0:
+        bg.setTexture(N);
+        break;
+      case 1:
+        bg.setTexture(E);
+        break;
+      case 2:
+        bg.setTexture(S);
+        break;
+      case 3:
+        bg.setTexture(W);
+        break;
     }
 
     window.clear();
-    window.draw(BackgroundSprite);
+    window.draw(bg);
     window.display();
   }
 
